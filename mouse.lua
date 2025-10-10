a3c0e2b7fd0999a0e12e63381cfdd850b04b75f2
Nathan <47441022+NathanSnail@users.noreply.github.com>
2025-10-11 02:25:31 +1300

    split up input enums

---
+++ mouse.lua
@@ -0,0 +1,13 @@
+-- mouse buttons to use with InputIsMouseButtonDown(...) API
+---@enum Mousebutton
+local M = {
+	Mouse_left = 1,
+	Mouse_right = 2,
+	Mouse_middle = 3,
+	Mouse_wheel_up = 4,
+	Mouse_wheel_down = 5,
+	Mouse_x1 = 6,
+	Mouse_x2 = 7,
+}
+
+return M
